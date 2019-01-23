ea08489a1fca9e07d69b9848d7474f1144314b3f
Chess <zhou.yiy@husky.neu.edu>
2019-01-23 10:18:47 -0500

    titleOnly

--- src/shared/student.model.ts
+++ src/shared/student.model.ts
@@ -1,5 +1,16 @@
-export class Student{
+export class Student {
   constructor(public name: string,
               public major: string) {
+  }
+}
+
+export class Material {
+  constructor(public name: string,
+              public type: string) {
+  }
+}
+
+export class Product {
+  constructor(public name: string) {
   }
 }
